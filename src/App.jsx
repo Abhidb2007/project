a590137c55c596725a3f06f18226aaf3a28a6175
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-25 19:23:29 +0530

    props name

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,14 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(1);
-
-  function increaseCount() {
-    setCount(count + 1);
-  }
-  
-
-  return (
-    <div>
-      
-        <div
-          style={{
-            borderRadius: 20,
-            backgroundColor: "pink",
-            padding: 10,
-            width: 40,
-            height: 40,
-            marginRight: 22,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontWeight: "bold",
-          }}
-        >
-          {count}
-        
-      </div>
-      <img
-        style={{ cursor: "pointer" }}
-        src="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg"
-        width={100}
-        
-      />
-      <br />
-      
-    </div>
-  );
+import{useState} from "react";
+function App(){
+  return <div>
+    hi heere
+    <Greeting name={"abhi"}/>
+  </div>
 }
+function Greeting(props){
+  return <div>
+    hi there{props.name}
 
+  </div>
+}
 export default App;
