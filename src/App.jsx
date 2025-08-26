a75e0a009ce72898f00160c1e927b6bbe069361d
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-26 05:53:54 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import{useState, useEffect} from "react";
+
 function App(){
-  const[count, setCount]=useState(1);
-  function increaseCount(){
+  const[currentTab, seCurrentTab]=useState("feed");
+  return <div>
+    <button onClick={function(){
+      setCurrentTab("feed")
+      }}  style={{color:currentTab=="feed"?"red":"yellow"}}>Feed</button>
     
-    setCount(function (currentCount){
-      return currentCount+1;
-    });
-
-  }
-  useEffect(function (){
-    console.log("above setinterval")
-    setInterval(increaseCount,1000)
-
-  },[count]);
-
-return <div>
-
-    <h1>Count:{count}</h1>
   </div>
-
 }
-export default App;
