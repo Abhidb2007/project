87d5933bb1ae2abeb8bf2da9f7927a8ff5a384ab
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-26 06:03:00 +0530

    btn click

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import { useState } from "react";
 function App(){
-  const[currentTab, seCurrentTab]=useState("feed");
+  const[currentTab, setCurrentTab]=useState("feed");
   return <div>
     <button onClick={function(){
       setCurrentTab("feed")
       }}  style={{color:currentTab=="feed"?"red":"yellow"}}>Feed</button>
+      <button onClick={function(){
+        setCurrentTab("message");
+      }}style={{color:currentTab=="message"? "red":"white"}}>message</button>
     
   </div>
 }
+export default App;
