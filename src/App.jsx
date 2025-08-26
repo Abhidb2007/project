7c63655a9f6017dededfc3a80a4c7ee5ac138588
Abhidb2007 <abhishekdb2210@gmail.com>
2025-08-26 06:16:31 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 function App(){
-  const[currentTab, setCurrentTab]=useState("feed");
+  const[currentTab, setCurrentTab]=useState(1);
+  useEffect(function(){
+    console.log("send request to backend to get data from tab"+currentTab)
+  },[currentTab])
   return <div>
     <button onClick={function(){
-      setCurrentTab("feed")
-      }}  style={{color:currentTab=="feed"?"red":"yellow"}}>Feed</button>
+      setCurrentTab("1")
+      }}  style={{color:currentTab=="1"?"red":"yellow"}}>Todo #1</button>
       <button onClick={function(){
-        setCurrentTab("message");
-      }}style={{color:currentTab=="message"? "red":"white"}}>message</button>
+        setCurrentTab("2");
+      }}style={{color:currentTab=="2"? "red":"white"}}>Todo #2</button>
     
   </div>
 }
